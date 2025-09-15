45928933468fa66729a4b2e56c8f0fb730201c84
garruka <dilanworf@gmail.com>
2025-09-15 16:07:05 -0300

    listo13

--- app1/views.py
+++ app1/views.py
@@ -1,7 +1,5 @@
 from django.http import HttpResponse
-
 def vista1(request):
-    return HttpResponse("<h1>hola<h1><a>desde la</a><p>la primera app2 y primera vista<p>")
-
+ return HttpResponse("<h1>App1 - Vista1</h1><p>Hola  App1/Vista1</p><a>todo bien</a>")
 def vista2(request):
-    return HttpResponse("<h1>hola<h1><a>desde la</a><p>la primera app2 y segunda vista<p>")
+ return HttpResponse("<h1>App1 - Vista2</h1><p>Hol App1/Vista2</p><a>todo bien</a>")
